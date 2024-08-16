95766bd5c9962655230b667c862d8382e3b101e7
Matheus <matheuscviana22@gmail.com>
2024-08-16 15:36:38 -0300

    testedeploy

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { HeroComponent } from "../hero/hero.component";
+import { ProjetosComponent } from "../projetos/projetos.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent],
+  imports: [NavbarComponent, HeroComponent, ProjetosComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
