4549551021d6be59718083153a8e2f4dddb45b3e
Matheus <matheuscviana22@gmail.com>
2024-08-17 15:26:41 -0300

    responsiveTecnologias

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 import { HeroComponent } from "../hero/hero.component";
 import { ProjetosComponent } from "../projetos/projetos.component";
+import { TecnologiasComponent } from "../tecnologias/tecnologias.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent, ProjetosComponent],
+  imports: [NavbarComponent, HeroComponent, ProjetosComponent, TecnologiasComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
