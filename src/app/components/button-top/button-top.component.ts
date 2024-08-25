b6a4291d6eb3a2a17549fd1e6de2946409611ddd
Matheus <matheuscviana22@gmail.com>
2024-08-25 10:23:02 -0300

    adicionadoMusicaNoSite

--- src/app/components/button-top/button-top.component.ts
+++ src/app/components/button-top/button-top.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-button-top',
   standalone: true,
@@ -8,11 +9,40 @@ import { Component } from '@angular/core';
   styleUrl: './button-top.component.css'
 })
 export class ButtonTopComponent {
-  
+
+  private audio: HTMLAudioElement;
+  isMuted: boolean = false;
+  isPlaying: boolean = false;
+
+  constructor() {
+    this.audio = new Audio();
+    this.audio.src = 'assets/sound/solas.mp3';
+    this.audio.loop = true;
+    this.audio.volume = 0.2;
+  }
+
   scrollToTop() {
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
     });
+  }
+
+  toggleMute() {
+    this.isMuted = !this.isMuted;
+    if (this.isMuted) {
+      this.audio.pause();
+    } else {
+      this.audio.play();
+    }
+  }
+
+  togglePlay() {
+    this.isPlaying = !this.isPlaying;
+    if (this.isPlaying) {
+      this.audio.play();
+    } else {
+      this.audio.pause();
+    }
   }
 }
